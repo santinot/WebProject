89e579dfe39f87333c0267941fa06d8ecf16e324
santinot <santinomoncata@gamil.com>
2023-01-07 16:51:31 +0100

    changes

--- home.php
+++ home.php
@@ -36,7 +36,7 @@ function LogOut(){
 
         <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
           <a class="me-3 py-2 text-dark text-decoration-none" href="home.php">Oggetti</a>
-          <a class="me-3 py-2 text-dark text-decoration-none" href="folders.php">Cartelle</a>
+          <a class="me-3 py-2 text-dark text-decoration-none" href="tools.php">Strumenti</a>
           <a class="me-3 py-2 text-dark text-decoration-none" href="insert.php">Inserisci</a>
           <a class="me-3 py-2 text-dark text-decoration-none nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">Profilo</a>
             <ul class="dropdown-menu">
