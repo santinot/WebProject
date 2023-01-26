7146a1d5c6d6c675b1242fbb2d8e994f71ad7a87
santinot <mncstn01e18f158l@studenti.unime.it>
2023-01-26 20:22:42 +0100

    ,,,

--- home.php
+++ home.php
@@ -66,13 +66,13 @@ if (!isset($_SESSION['ID_User'])) {
                   <img src="img/list-check.svg" class="bi bi-list-check mx-3" width="30" height="30">Tutti gli oggetti..</button>
                 </li>
                 <li class="list-group-item"><button class="btn btn-outline-dark" id="loginBtn" value="ItemLogin">
-                  <img src="img/login.svg" class="bi bi-list-check mx-2" width="30" height="30">Login</button>
+                  <img src="img/login.svg" class="bi mx-2">Login</button>
                 </li>
                 <li class="list-group-item"><button class="btn btn-outline-dark" id="cardBtn" value="ItemCard">
-                  <img src="img/card.svg" class="bi bi-list-check mx-2" width="30" height="30">Carte</button>
+                  <img src="img/card.svg" class="bi mx-2">Carte</button>
                 </li>
                 <li class="list-group-item"><button class="btn btn-outline-dark" id="noteBtn" value="ItemNote">
-                  <img src="img/note.svg"  class="bi bi-list-check mx-2" width="30" height="30">Note</button>
+                  <img src="img/note.svg"  class="bi mx-2">Note</button>
                 </li>
               </ul>
             </div>
@@ -92,6 +92,21 @@ if (!isset($_SESSION['ID_User'])) {
           <div class="col-sm-8 text-center" id="division">
           <table id="myTable"></table>
 
+          <ul class="list-group" id="homeList">
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              <img src="img/login.svg" class="bi">
+              A list item
+              <span class="badge bg-primary rounded-pill">14</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              A second list item
+              <span class="badge bg-primary rounded-pill">2</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              A third list item
+              <span class="badge bg-primary rounded-pill">1</span>
+            </li>
+        </ul>
 
 
           </div>
